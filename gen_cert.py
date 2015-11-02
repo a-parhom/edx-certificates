61705f8dc1010ea292ff0dc4f35f552ae6baa04d
Oleksii <olexiim@gmail.com>
2015-11-02 23:55:35 +0200

    Added IRF/101/2015_T2 certificates

--- gen_cert.py
+++ gen_cert.py
@@ -905,7 +905,7 @@ class CertificateGen(object):
         # Subscription for certain courses
         if self.course=='101' and self.org=='irf':
             styleOpenSansLight.fontSize = 10
-            paragraph_string = 'Курс виготовлено в межах "Ініціативи з розвитку аналітичних центрів в Україні", яку виконує МФ "Відродження" <br>у партнерстві з Фондом розвитку аналітичних центрів (TTF) за фінансової підтримки посольства Швеції в Україні (SIDA).'
+            paragraph_string = 'Курс виготовлено в межах "Ініціативи з розвитку аналітичних центрів в Україні", яку виконує МФ "Відродження" <br />у партнерстві з Фондом розвитку аналітичних центрів (TTF) за фінансової підтримки посольства Швеції в Україні (SIDA).'
             paragraph = Paragraph(paragraph_string, styleOpenSansLight)
             paragraph.wrapOn(c, WIDTH * mm, HEIGHT * mm)
             paragraph.drawOn(c, LEFT_INDENT * mm, 70 * mm)
